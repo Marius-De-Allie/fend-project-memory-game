6acaffe0154aa279a82b1b674aedc1c0931c1568
Spectre4300s <spectre4300s@yahoo.com>
2019-04-21 15:25:18 -0400

    Clean up Code, remove whitespace.

--- js/app.js
+++ js/app.js
@@ -161,6 +161,7 @@ function resetPlayerMoves() {
   const movesContent = document.querySelector('.moves');
   movesContent.innerHTML = playerMoves;
 }
+
 // Function to reset game clock.
 function resetClock() {
   stopTimer();
@@ -234,13 +235,3 @@ modalNewGame.addEventListener('click', function() {
 const restart = document.querySelector('.restart i');
 //restart button click event listener.
 restart.addEventListener('click', newGame);
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
