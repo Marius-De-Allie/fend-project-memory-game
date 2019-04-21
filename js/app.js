0dc8ef5ef0adb802cf81dc201d652b773cfb456f
Spectre4300s <spectre4300s@yahoo.com>
2019-04-20 22:55:24 -0400

    Declared var to hold total number of pairs of cards in deck and assigned it val of 8.

--- js/app.js
+++ js/app.js
@@ -47,9 +47,10 @@ const modalClose = document.querySelector('.close_button');
 const modalNewGame = document.querySelector('#restart_button');
  // select all star elements from DOM and assign to variable.
 const totalStars = [...document.querySelectorAll('.stars li')];
-// dedclare variable to keep count of cards that are matched.
+// declare variable to keep count of cards that are matched.
 let matchedCards = 0;
-
+// Variable to hold number of pairs of cards in deck.
+const allCardPairs = 8;
 // Function to shuffle deck of cards
 function shuffleCards() {
   let cardsShuffled = shuffle(allCards);
